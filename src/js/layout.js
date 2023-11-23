9bd4918a6481746a5ac4aabe72e9a6fdd63aa127
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-11-23 18:30:00 +0100

    layout.js

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,30 +2,31 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { Home } from "./views/home";;
 import injectContext from "./store/appContext";
+import { NewContact } from "./views/NewContact";
 
 import { Navbar } from "./component/navbar";
-import { Contactlist } from "./component/contactlist";
-import { Newcontact } from "./views/newcontact";
+import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
+	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
 
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Contactlist />
-					<Newcontact />
+					<Navbar />
 					<Routes>
-						<Route path="/" element={<Contactlist />} />
-						<Route path="/newcontact" element={<Newcontact />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
+						<Route path="/" element={<Home />} />
+						<Route path="/new-contact" element={<NewContact />} />
+						
+
 					</Routes>
+					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
